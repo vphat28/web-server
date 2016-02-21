e8b0dcfd777c8a9fc80ae51ba0255a715a30758e
Xavi Nguyen <vphat28@gmail.com>
2016-02-21 11:17:59 +0700

    change middleware

---
+++ middleware.js
@@ -0,0 +1,11 @@
+var middleware = {
+    requireAuthentication: function (reg, res, next) {
+        console.log('private route hit!');
+    },
+    logger: function (reg, res, next) {
+        console.log('Regquest: ' + new Date().toString() + ' ' + reg.method + ' ' + reg.originalUrl);
+        next();
+    }
+};
+
+module.exports = middleware;
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var PORT = 3000;
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
 
 app.get('/', function(req, res) { 
     res.send('Hello NodeJs World');
